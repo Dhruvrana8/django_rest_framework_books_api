155fbcb2387f52b372c82ef153672b3da441cfb7
Dhruv Rana <dhruvrana4@gmail.com>
2024-05-19 22:30:15 +0530

    feat: add Delete methord to remove any book

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .models import Books
 from .serializers import BooksSerializer
 
+
 class BooksAPIView(APIView):
     def get(self, request):
         data = Books.objects.all()
@@ -15,28 +16,38 @@ class BooksAPIView(APIView):
         else:
             return Response(serializer.errors())
 
-    def post(self,request):
-        data= request.data
-        serializer = BooksSerializer(data= data)
-        
+    def post(self, request):
+        data = request.data
+        serializer = BooksSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-        
+
     def put(self, request):
         book_id = request.data.get('id')
-        
+
         if not book_id:
             return Response({"error": "ID is required to update a book."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         book = get_object_or_404(Books, id=book_id)
 
         serializer = BooksSerializer(book, data=request.data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        book_id = request.data.get('id')
+
+        if not book_id:
+            return Response({"error": "ID is a required field to delete the book"}, status=status.HTTP_400_BAD_REQUEST)
+
+        book = get_object_or_404(Books, id=book_id)
+        book.delete()
+        return Response({"message":"The Book had been deleted"})
